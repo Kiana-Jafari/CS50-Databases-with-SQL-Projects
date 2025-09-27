f7dd1d66b7bc4b4e7e3063b7297bbfa50bcd2cb2
Kiana Jafari <kianapiano5@gmail.com>
2025-09-27 17:40:22 +0330

    Create 6.sql

---
+++ Problem Set 1 - Relating/DESE/6.sql
@@ -0,0 +1,26 @@
+-- DESE wants to assess which schools achieved a 100% graduation rate.
+-- In 6.sql, write a SQL query to find the names of schools (public or charter!) that reported a 100% graduation rate.
+
+SELECT name
+FROM schools
+WHERE id IN (
+    SELECT school_id
+    FROM graduation_rates
+    WHERE graduated = 100
+);
+
+/*
++--------------------------------------------------------+
+|                          name                          |
++--------------------------------------------------------+
+| Tahanto Regional High                                  |
+| O'Bryant School of Math & Science                      |
+| Cohasset High School                                   |
+| Global Learning Charter Public School                  |
+| Bromfield                                              |
+| Pioneer Valley Regional                                |
+| Sizer School: A North Central Charter Essential School |
+| Upper Cape Cod Vocational Technical                    |
+| Weston High                                            |
++--------------------------------------------------------+
+*/
