bc271c1bac789fed3f2a599fc985d80516d4d0b3
Kiana Jafari <kiana.jafari.ai@gmail.com>
2025-10-11 16:16:15 +0330

    Update hack.py

--- Problem Set 6 - Scaling/Don't Panic - Python/hack.py
+++ Problem Set 6 - Scaling/Don't Panic - Python/hack.py
@@ -11,3 +11,37 @@ db.execute(
 
     """, password
     );
+
+________________________________________________________________________________
+
+ # Alternative approach:
+
+# Get the password from the user.
+password = input("Enter your password: ")
+
+# Import the sqlite3 package.
+import sqlite3
+
+# Make a connection to the SQLite3 server.
+connection = sqlite3.connect("dont-panic.db")
+
+# Create a cursor object: This is mainly for writing SQL queries.
+cursor_obj = connection.cursor()
+
+# Execute the code using the `.execute` method on the object.
+cursor_obj.execute(
+    """
+    UPDATE "users"
+    SET "password" = ?
+    WHERE "username" = 'admin'
+
+    """, [password]
+)
+
+# Use the `.commit` method so that the update persists.
+connection.commit()
+
+# Close the connection.
+connection.close()
+    
+    
